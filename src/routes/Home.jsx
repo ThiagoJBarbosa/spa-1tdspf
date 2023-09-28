3b03c61265afdf3a105959d9cff06ed1d9af31ee
ThiagoJBarbosa <thiagojesusbs@gmail.com>
2023-09-28 10:44:47 -0300

    Removendo os arquivos que não estao sendo usados e adicionando imagem

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,6 +2,10 @@ export default function Home() {
   return (
     <>
         <h1>Home</h1>
+        <figure>
+          <figcaption>Nossos Produtos</figcaption>
+          <img src="/img/produtos_home.jpg" alt="Produtos" />
+        </figure>
     </>
   )
 }
